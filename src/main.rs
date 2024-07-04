758307bad6867fa222cbc89344a1790384c00b23
Matt Williams <matt@milliams.com>
2024-07-04 12:42:36 +0100

    Make release flow only tag once

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 // SPDX-FileCopyrightText: © 2024 Matt Williams <matt.williams@bristol.ac.uk>
 // SPDX-License-Identifier: MIT
 
-fn main() {}
+use clap::Parser;
+
+pub mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+#[derive(Parser)]
+#[command(version = built_info::GIT_VERSION.expect("Could not find version"))]
+struct Args {}
+
+fn main() {
+    let _ = Args::parse();
+}
